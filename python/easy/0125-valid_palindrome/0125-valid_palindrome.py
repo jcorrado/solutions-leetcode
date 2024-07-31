488a7416421e086fe78676212ce547eb0c1e4f8a
Jereme Corrado <jereme@zoion.net>
2024-07-31 14:12:29 -0400

    Add more Py 125 ordinal perf tweaking via ordinal values

--- python/easy/0125-valid_palindrome/0125-valid_palindrome.py
+++ python/easy/0125-valid_palindrome/0125-valid_palindrome.py
@@ -8,33 +8,47 @@ include letters and numbers.
 
 Given a string s, return true if it is a palindrome, or false
 otherwise.
- 
+
 Example 1:
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.
 """
 
-import re
 
-
+# 0-9: 48-57, a-z: 97-122
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        def to_ord(c):
+            x = ord(c)
+            if 65 <= x <= 90:
+                # A-Z, convert to lower case
+                return x + 22
+            elif 48 <= x <= 57 or 97 <= x <= 122:
+                # 0-9 or a-z
+                return x
+            else:
+                return None
+
         s = s.lower()
         i, j = 0, len(s) - 1
         while i <= j:
-            ci = ord(s[i])
-            cj = ord(s[j])
-            # 0-9: 48-57, a-z: 97-122
-            if not ((48 <= ci <= 57) or (97 <= ci <= 122)):
+
+            ci = to_ord(s[i])
+            if ci is None:
                 i += 1
-            elif not ((48 <= cj <= 57) or (97 <= cj <= 122)):
+                continue
+
+            cj = to_ord(s[j])
+            if cj is None:
                 j -= 1
-            else:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
+                continue
+
+            if ci != cj:
+                return False
+            i += 1
+            j -= 1
+
         return True
 
 
