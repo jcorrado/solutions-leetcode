71b253137acdf86db0668544e8682a4b51293f2a
Jereme Corrado <jereme@zoion.net>
2024-07-31 14:28:01 -0400

    Py: 125 Valid Palindrome, fast enough

--- python/easy/0125-valid_palindrome/0125-valid_palindrome.py
+++ python/easy/0125-valid_palindrome/0125-valid_palindrome.py
@@ -16,39 +16,19 @@ Explanation: "amanaplanacanalpanama" is a palindrome.
 """
 
 
-# 0-9: 48-57, a-z: 97-122
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        def to_ord(c):
-            x = ord(c)
-            if 65 <= x <= 90:
-                # A-Z, convert to lower case
-                return x + 22
-            elif 48 <= x <= 57 or 97 <= x <= 122:
-                # 0-9 or a-z
-                return x
-            else:
-                return None
-
-        s = s.lower()
         i, j = 0, len(s) - 1
-        while i <= j:
-
-            ci = to_ord(s[i])
-            if ci is None:
+        while i < j:
+            if not s[i].isalnum():
                 i += 1
-                continue
-
-            cj = to_ord(s[j])
-            if cj is None:
+            elif not s[j].isalnum():
                 j -= 1
-                continue
-
-            if ci != cj:
+            elif s[i].lower() != s[j].lower():
                 return False
-            i += 1
-            j -= 1
-
+            else:
+                i += 1
+                j -= 1
         return True
 
 
