aaef24293ba2b67c94e0931059dcbcd7acfb043f
Jereme Corrado <jereme@zoion.net>
2024-07-31 13:48:53 -0400

    Py 125: speedup via ordinal value inspection chicanery

--- python/easy/0125-valid_palindrome/0125-valid_palindrome.py
+++ python/easy/0125-valid_palindrome/0125-valid_palindrome.py
@@ -20,14 +20,21 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub(r"(?i)[^a-z0-9]", "", s.lower())
-        i = 0
-        j = len(s) - 1
+        s = s.lower()
+        i, j = 0, len(s) - 1
         while i <= j:
-            if s[i] != s[j]:
-                return False
-            i += 1
-            j -= 1
+            ci = ord(s[i])
+            cj = ord(s[j])
+            # 0-9: 48-57, a-z: 97-122
+            if not ((48 <= ci <= 57) or (97 <= ci <= 122)):
+                i += 1
+            elif not ((48 <= cj <= 57) or (97 <= cj <= 122)):
+                j -= 1
+            else:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
         return True
 
 
