f1ededbfd47903680ab673269490a0c43d8fc715
Jereme Corrado <jereme@zoion.net>
2024-08-01 10:37:59 -0400

    Add Py: 205 Isomorphic Strings.  Slow first solution.

---
+++ python/easy/0205-isomorphic_strings/0205-isomorphic_strings.py
@@ -0,0 +1,62 @@
+"""
+https://leetcode.com/problems/isomorphic-strings/
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be
+replaced to get t.
+
+All occurrences of a character must be replaced with another character
+while preserving the order of characters. No two characters may map to
+the same character, but a character may map to itself.
+
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true
+"""
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_len = len(s)
+        t_len = len(t)
+
+        if s_len != t_len:
+            return False
+
+        mappings, used = {}, set()
+        i = 0
+        while i < s_len:
+            cs = s[i]
+            ct = t[i]
+            if cs in mappings:
+                if mappings[cs] != ct:
+                    return False
+            else:
+                if ct in used:
+                    return False
+                else:
+                    mappings[cs] = ct
+                    used.add(ct)
+            i += 1
+
+        return True
+
+
+print(Solution().isIsomorphic("egg", "add"))
+# True
+print(Solution().isIsomorphic("foo", "bar"))
+# False
+print(Solution().isIsomorphic("paper", "title"))
+# True
+print(Solution().isIsomorphic("babc", "baba"))
+# False
